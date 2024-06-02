7ac2b3b1ddcb756f2447b1f041a01d50a6468016
keni <kenirjain2002@gmail.com>
2024-06-03 01:33:38 +0530

    Added multiparty

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -29,6 +29,20 @@ export default function ProductForm({
     if(goToProducts){
         router.push('/products');
     }
+    async function uploadImages(ev){
+        const files= ev.target?.files;
+        if(files?.length>0){
+            const data = new FormData();
+            for (const file of files ){
+                data.append('file', file);
+            }
+            const res = await fetch('/api/upload', {
+                method : 'POST',
+                body : data,
+            })
+            console.log(res);
+        }
+    }
     return (
             <form onSubmit={saveProduct}>
             <label>Product Name</label>
@@ -39,14 +53,15 @@ export default function ProductForm({
                 onChange={ev => setTitle(ev.target.value)}/>
             <label>Photos</label>
             <div className="mb-2">
-                <button className="w-24 h-24 text-center flex items-center justify-center text-s gap-1 text-gray-500 rounded-lg bg-gray-200">
+                <label className="w-24 h-24 cursor-pointer text-center flex items-center justify-center text-s gap-1 text-gray-500 rounded-lg bg-gray-200">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
                     </svg>
                     <div>
                         Upload
                     </div>
-                </button>
+                    <input type="file" onChange={uploadImages} className="hidden"/>
+                </label>
                 {!images?.length && (
                     <div>No photos in this product</div>
                 )}
